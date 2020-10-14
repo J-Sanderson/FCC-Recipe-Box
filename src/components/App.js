2b281bbb644839e0921d05be5380cdb1ff06cab8
J Sanderson <ju.a.sanderson@gmail.com>
2020-10-14 17:58:50 +0100

    feat: display default recipe list

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import RecipeList from './RecipeList';
+
+import '../index.css'
+
+class App extends Component {
+
+  state = {
+    recipes: [],
+  };
+
+  componentDidMount = () => {
+    const defaults = [
+      {name: 'Chocolate Cake', ingredients: ['flour', 'sugar', 'eggs', 'cocoa powder']},
+      {name: 'Fresh Bread', ingredients: ['flour', 'yeast']},
+      {name: 'Fish and Chips', ingredients: ['fish', 'chips']}
+    ]
+    this.setState({
+      recipes: defaults,
+    })
+  };
+
+  render() {
+    return (
+      <main>
+        <h1>Recipe Box</h1>
+        <RecipeList recipes={this.state.recipes} />
+        <button id="add-recipe">
+            Add Recipe
+        </button>
+      </main>
+    );
+  };
+
+}
+
+export default App;
