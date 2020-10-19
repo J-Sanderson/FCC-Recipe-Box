6a1a1cebdd25b85ad74f8b053da69311165e4dd4
J Sanderson <ju.a.sanderson@gmail.com>
2020-10-19 14:55:12 +0100

    feat: set up recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -7,10 +7,18 @@ class Recipe extends Component {
     };
 
     render() {
+        const ingredients = this.props.ingredients.map((ingredient, index) => {
+            return (
+                <li key={index}>
+                    {ingredient}
+                </li>
+            );
+        });
+
         return (
             <div className="recipe">
                 <h2 className="recipe-title">{this.props.name}</h2>
-                <div className="ingredients">{this.props.ingredients.join(', ')}</div>
+                <ul className="ingredients">{ingredients}</ul>
             </div>
         );
     };
