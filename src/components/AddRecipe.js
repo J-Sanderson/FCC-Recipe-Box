5abebb9b1aa20720195f9715f48be38deabfde31
J Sanderson <ju.a.sanderson@gmail.com>
2020-10-30 14:22:09 +0000

    style: add recipe popup

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -3,10 +3,30 @@ import React, { Component } from 'react';
 import '../css/AddRecipe.css';
 
 class AddRecipe extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    handleCancel = () => {}
+
     render(){
         return (
             <div className="add-recipe">
                 <h2>Add Recipe</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <span>Name:</span>
+                        <input type='text' id='add-name' required ref='newName'/>
+                    </label>
+                    <label>
+                        <span>Ingredients (seperated by commas, e.g. "eggs, milk, flour"):</span>
+                        <input type='text' id='add-ingredients' required ref='newIngredients'/>
+                    </label>
+                    <input type='submit' id='add-submit' value='Add recipe' />
+                </form>
+                <button onClick={this.handleCancel}>
+                    Cancel
+                </button>
             </div>
         );
     };
